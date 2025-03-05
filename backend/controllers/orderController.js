b8358a50ae8f352864ee90c3ecd18a174f73107f
Wijeyaabhimanue Wijeyamohan <161345814+wijeyaabhimanue0107@users.noreply.github.com>
2025-03-05 23:29:18 +0530

    Product list, Single product by id, order API Complete

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,24 @@
-exports.createOrder = (req, res, next) => {
+const orderModel = require('../models/orderModel');
+const productModel = require('../models/productModel');
+
+
+//Create Order API - /api/v1/order
+exports.createOrder = async (req, res, next) => {
+    const cartItems = req.body;
+    const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0).toFixed(2));
+    const status = "pending"
+    const order = await orderModel.create({ cartItems, amount, status });
+
+
+    //Updating Product Stocks
+    // cartItems.forEach(async (item) => {
+    //     const product = await productModel.findById(item.product._id)
+    //     product.stock = product.stock - item.qty;
+    //     await product.save();
+    // });
+
     res.json({
         success: true,
-        message: 'Order Working!'
+        order
     });
 }
--- backend/controllers/productCntroller.js
+++ backend/controllers/productCntroller.js
@@ -1,12 +1,27 @@
-exports.getProducts = (req, res, next) => {
+const productModel = require('../models/productModel');
+
+//Get Products API - /api/v1/products
+exports.getProducts = async (req, res, next) => {
+    const products = await productModel.find({})
     res.json({
         success: true,
-        message: 'Get Products Working!'
+        products
     });
 }
-exports.getSingleProducts = (req, res, next) => {
-    res.json({
-        success: true,
-        message: 'Get Single Products Working!'
-    });
+
+//Get Single Products API - /api/v1/products/:id
+exports.getSingleProducts = async (req, res, next) => {
+    try {
+        const product = await productModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        });
+    }
+    catch(error) {
+        res.status(404).json({
+            success: true,
+            message: error.message ? error.message : 'Product not found'
+        });
+    }
 }
